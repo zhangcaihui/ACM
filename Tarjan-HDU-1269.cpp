8f1248bd509ec77a3016121737fbfb35c507beb1
hexisyztem <hexisyztem@icloud.com>
2017-10-18 16:44:40 +0800

    HDU-1269

---
+++ Tarjan-HDU-1269.cpp
@@ -0,0 +1,51 @@
+nclude<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+using namespace std;
+const int maxn = 10010;
+vector<int> pp[maxn];
+
+int low[maxn], dfn[maxn], cnt;
+
+void Tarjan(int x){
+    low[x] = dfn[x] = ++ cnt;
+    int k = pp[x].size();
+    for(int i = 0; i < k; i ++){
+        int v = pp[x][i];
+        if(!dfn[v]){
+            Tarjan(v);
+        }
+        low[x] = min(low[x], low[v]);
+    }
+}
+
+int main(){
+    int n, m;
+    while(scanf("%d%d", &n, &m) && n){
+        int x, y, ff = 1;
+        for(int i = 1; i <= m; i ++){
+            scanf("%d%d", &x, &y);
+            pp[x].push_back(y);
+        }
+        
+        Tarjan(1);
+        
+        for(int i = 1; i <= n; i ++)
+            if(low[i] != 1)
+                ff = 0;
+        
+        if(ff)
+            printf("Yes\n");
+        else
+            printf("No\n");
+        
+        
+        for(int i = 1; i <= n; i ++)
+            pp[i].clear();
+        memset(low, 0, sizeof(low));
+        memset(dfn, 0, sizeof(dfn));
+        cnt = 0;
+    }
+}
+
