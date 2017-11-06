de76173c54b6a092a9ce05c6b1c8dbe2225dd220
UESTC_Duiming <uestc_duiming@xian.icpc>
2017-11-06 15:01:14 +0800

    Daily commit -06/11/17

---
+++ Algorithm/Graph/Kruskal.cpp
@@ -0,0 +1,21 @@
+struct edge{
+    int x, y, val;
+    friend bool operator < (edge xx, edge yy){
+        return xx.val < yy.val;
+    }
+}e[maxn];
+
+int find(int x){ return f[x] == -1 ? x : (f[x] = find(f[x]));}
+
+int Kruskal(){
+    for(int i = 1; i <= m; i ++){
+        int xx =find(e[i].x), yy = find(e[i].y);
+        if(xx == yy)
+            continue;
+        f[xx] = yy;
+    }
+    for(int i = 2; i <= n; i ++)
+        if(find(1) != find(i))   
+           return -1;// can't connet
+    return 1; // can connect
+}
