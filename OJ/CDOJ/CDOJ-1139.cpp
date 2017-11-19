a2eea013d9696eb176b766b8b0105ccce09c7cfe
hexisyztem <hexisyztem@icloud.com>
2017-11-19 11:49:52 +0800

    move files to correct position

---
+++ OJ/CDOJ/CDOJ-1139.cpp
@@ -0,0 +1,11 @@
+#include<algorithm>
+#include<cstdio>
+using namespace std;
+
+long long int a[1000];
+int main(){
+    a[0] = a[1] = 1;
+    for(int i = 2; a[i-1] <= 1e5; i ++){
+        
+    }
+}
