f9180080fa3faf2b7eb6dee06f8063cd0487e08a
UESTC_Duiming <uestc_duiming@xian.icpc>
2017-12-21 11:08:36 +0800

    Homework 1st

--- Homework/1st-Experiment/Prob2.cpp
+++ Homework/1st-Experiment/Prob2.cpp
@@ -4,33 +4,37 @@ using namespace std;
 const int maxn = 2333333;
 struct node{
     int num;
+    int jum;
     node *next;
 }*a[maxn];
 int main(){
     int n, m;
     scanf("%d%d", &n, &m);
     /*  debug   */
-    printf("note\n");
     for(int i = 0; i <= n; i ++)
         a[i] = (node *)malloc(sizeof(node));
-    printf("%ud compared with %ud\n", sizeof(node), sizeof(node*));
     for(int i = 0; i < n; i ++)
         a[i]->next = a[i+1];
     a[n]->next = a[1];
     /*  debug   */
     
     for(int i = 1; i <= n; i ++)
-        scanf("%d", &a[i]->num);
+        scanf("%d", &a[i]->jum), a[i]->num = i;
     int cnt = n;
     node *p = a[0];
     while(cnt){
         int k = m % cnt;
-        for(int i = 1; i <= k; i ++){
+        for(int i = 1; i < k; i ++){
             p = p->next;
         }
         node * tem = p->next;
-        p->next = p->next->next;
-        printf("%d ", p->num);
+        p->next = tem->next;
+        m = tem->jum;
+        printf("%d ", tem->num);
         cnt --;
     }
 }
+/*
+7 20
+3 1 7 2 4 8 4
+*/
